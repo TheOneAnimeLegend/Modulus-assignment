5155b8551980dca021d8cc72417af5cac837d709
TheOneAnimeLegend <43045937+TheOneAnimeLegend@users.noreply.github.com>
2018-10-03 14:09:06 -0600

    Update

---
+++ index.js
@@ -0,0 +1,11 @@
+let number = [10, 75]; //Boundary Numbers
+let skipCount = 8; //Number of skipCounting between each element of the sequence, used in FOR Loop
+let difference = number[] - number[]; //Difference, mandatory expectation
+let startingNumber = number[]; //Hint: used in the FOR Loop
+for (let i=0; i<=difference; i++){
+  even = (startingNumber += skipCount) % 2;
+  console.log("This number is", even, "; This iteration is number", i+skipCount); //Example of Concatenating
+}
+
+console.log(number){
+console.log(skipcount);
